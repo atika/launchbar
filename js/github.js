f1dc600aec4895cf6888973afed951ea0df7ec7f
Dominique Da Silva <dom@inspira.io>
2016-01-28 16:58:42 +0100

    Site updated: 2016-01-28 16:58:42

---
+++ js/github.js
@@ -0,0 +1,22 @@
+
+var winSize = '';
+
+function winUpdate() {
+	var newWinSize = 'xs';
+	if ($(this).width() >= 1200) { newWinSize = 'lg'; }
+	else if ($(this).width() >= 992) { newWinSize = 'md'; }
+	else if ($(this).width() >= 768) { newWinSize = 'sm'; }
+
+	if( newWinSize !== winSize ) {
+		winSize = newWinSize;
+
+		if (winSize === "xs") {
+			$(".col-left, .col-right").height("");
+		}else{
+			$(".col-left, .col-right").height(Math.max($(".col-left").height(), $(".col-right").height()));
+		}
+	}
+}
+
+window.onresize = winUpdate;
+winUpdate();
